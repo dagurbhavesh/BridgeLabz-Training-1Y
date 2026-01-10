8ea63e2851f7cc0abdbf46a9ece8e30c2b991319
BHAVESH <dagurbhavesh24@gmail.com>
2026-01-10 20:46:58 +0530

    TESTING

---
+++ basic-programs/self5.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class self5 {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("kilometer = ");
+        int kilometer = sc.nextInt();
+        double miles = kilometer * 0.621371;
+        System.out.println(miles);
+
+
+    }
+ }
