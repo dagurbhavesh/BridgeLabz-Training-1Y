88982a4a27cf663ceb293950132062056d73c523
BHAVESH <dagurbhavesh24@gmail.com>
2026-01-10 23:35:37 +0530

    Added area of circle

---
+++ basic-programs/areaofcircle.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+    public class areaofcircle {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int redius = sc.nextInt();
+        double area = 3.14 * (redius*redius) ;
+        System.out.println(area);
+
+    }
+}
